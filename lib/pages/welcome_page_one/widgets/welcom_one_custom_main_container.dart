93c1928753dd57cbd9f20590e9cccc8408c532c7
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-08-05 12:49:54 +0200

    edit welcom one page

--- lib/pages/welcome_page_one/widgets/welcom_one_custom_main_container.dart
+++ lib/pages/welcome_page_one/widgets/welcom_one_custom_main_container.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:test_mangement/utilites/assets.dart';
+import 'package:test_mangement/utilites/extentionhelper.dart';
 import 'package:test_mangement/utilites/widgets/customtext.dart';
 
 import '../../../generated/l10n.dart';
 import '../../../utilites/appcolors.dart';
+import '../../authPages/loginpage/login_page.dart';
 import 'custom_sub_container.dart';
 
 class WelcomOneCustomMainContainer extends StatelessWidget {
@@ -20,58 +22,64 @@ class WelcomOneCustomMainContainer extends StatelessWidget {
           borderRadius: BorderRadius.circular(25),
           color: AppColor.whiteColor,
         ),
-        child: ListView(
-          children: [
-            const SizedBox(
-              height: 14,
-            ),
-            CustomTextarabic(
-              text: S.of(context).Login,
-              fontSize: 22,
-              fontWeight: FontWeight.w700,
-            ),
-            CustomTextarabic(
-              text: S.of(context).chooserole,
-              fontSize: 22,
-              fontWeight: FontWeight.w700,
-            ),
-            //نص اخر دورك
-            Padding(
-              padding: const EdgeInsets.only(right: 30.0, top: 80),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      CustomSubContainer(
-                        containerText: S.of(context).Student, //ضيف الاسم الصح
-                        color: AppColor.primary,
-                        image: AssetsData.student,
-                      ),
-                      CustomSubContainer(
-                        containerText: S.of(context).Guardian, //ضيف الاسم الصح
-                        color: AppColor.lightGreenColor,
-                        image: AssetsData.parent,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      CustomSubContainer(
-                        containerText: S.of(context).Teached, //ضيف الاسم الصح
-                        color: AppColor.pinkLight,
-                        image: AssetsData.father,
-                      ),
-                      CustomSubContainer(
-                        containerText: S.of(context).School, //ضيف الاسم الصح
-                        color: AppColor.braon,
-                        image: AssetsData.young,
-                      ),
-                    ],
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 14,
+
               ),
-            ),
-          ],
+              CustomTextarabic(
+                text: S.of(context).Login,
+                fontSize: 22,
+                fontWeight: FontWeight.w700,
+              ),
+              CustomTextarabic(
+                text: S.of(context).chooserole,
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+          
+              Padding(
+                padding: const EdgeInsets.only(right: 30.0, top: 30),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        CustomSubContainer(
+                          onTap : (){
+                            context.push(LoginView());
+                          },
+                          containerText: S.of(context).Student, //ضيف الاسم الصح
+                          color: AppColor.primary,
+                          image: AssetsData.student,
+                        ),
+                        CustomSubContainer(
+                          containerText: S.of(context).Guardian, //ضيف الاسم الصح
+                          color: AppColor.lightGreenColor,
+                          image: AssetsData.parent,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        CustomSubContainer(
+                          containerText: S.of(context).Teached, //ضيف الاسم الصح
+                          color: AppColor.pinkLight,
+                          image: AssetsData.father,
+                        ),
+                        CustomSubContainer(
+                          containerText: S.of(context).School, //ضيف الاسم الصح
+                          color: AppColor.braon,
+                          image: AssetsData.young,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
