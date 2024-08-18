eb852874efbe7e691256e26177b7b4881cb1a2a6
mahmoudrabie97 <mahmoudrabea082@gmail.com>
2024-08-18 10:53:48 +0300

    o

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:test_mangement/cubit/authcubit/authstates.dart';
 import 'package:test_mangement/generated/l10n.dart';
 import 'package:test_mangement/pages/create_group/create_group_page.dart';
 import 'package:test_mangement/pages/events_page/events_page.dart';
+import 'package:test_mangement/pages/group_quize/group_quize_page.dart';
 import 'package:test_mangement/pages/notifications/notifications_page.dart';
 import 'package:test_mangement/pages/oneToOne_quiz/summary_one_to_one/summary_one_to_one.dart';
 import 'package:test_mangement/pages/quantitive/quantitive.dart';
@@ -46,7 +47,7 @@ class MyApp extends StatelessWidget {
               bottomNavigationBarTheme: const BottomNavigationBarThemeData(
                 backgroundColor: AppColor.primary,
               )),
-          home: CreateGroupPage()),
+          home: WelcomePage1()),
     );
   }
 }
